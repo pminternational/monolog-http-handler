abc343e5117aba39e3a5fd06f7b0f936453bcb31
Markus Schlotbohm <markus_schlotbohm@as-gmbh.de>
2018-03-21 14:55:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Monolog Http Handler
+
+This package provides a HttpHandler for the [Monolog](https://github.com/Seldaek/monolog) library.
+
+Prerequisites
+-------------
+
+- PHP 7.0 or above.
+- Since this package adds a HttpHandler to the [Monolog](https://github.com/Seldaek/monolog) library, you should first install [Monolog](https://github.com/Seldaek/monolog#installation).
+
+Installation
+------------
+
+Install the latest version with
+
+```bash
+$ composer require msschl/monolog-http-handler
+```
+
+#### After the installation
+
+...you need to decide on which [HTTP client/adapter](https://packagist.org/providers/php-http/client-implementation) you want to use.
+
+##### HTTP Clients
+
+In order to send HTTP requests, you need a HTTP adapter. This package relies on HTTPlug which is build on top of [PSR-7](https://www.php-fig.org/psr/psr-7/)
+and defines how HTTP message should be sent and received. You can use any library to send HTTP messages that
+implements [php-http/client-implementation](https://packagist.org/providers/php-http/client-implementation).
+
+Here is a list of all officially supported clients and adapters by HTTPlug: http://docs.php-http.org/en/latest/clients.html
+
+Read more about HTTPlug in [their docs](http://docs.php-http.org/en/latest/httplug/users.html).
+
+Basic Usage
+-----------
+
+```php
+<?php
+
+use Monolog\Logger;
+use Msschl\Monolog\Handler\HttpHandler;
+
+// create a log channel
+$log = new Logger('name');
+
+// push the HttpHandler to the monolog logger.
+$log->pushHandler(new HttpHandler([
+    'uri'     => 'https://localhost/your/endpoint/for/logging',
+    'method'  => 'POST',
+]));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+```
+
+About
+-----
+
+### Submitting bugs and feature requests
+
+Bugs and feature request are tracked on [GitHub](https://github.com/msschl/monolog-http-handler/issues)
+
+### Contributing
+
+First of all, **thank you** for contributing!
+In order to make code reviews easier please follow some simple rules listed in the [CONTRIBUTING.md](CONTRIBUTING.md) file.
+
+License
+-------
+
+This project is licensed under the terms of the MIT license.
+See the [LICENSE](LICENSE.md) file for license rights and limitations.
---
+++ src/HttpHandler.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace Msschl\Monolog\Handler;
+
+use Http\Client\HttpAsyncClient;
+use Http\Discovery\HttpAsyncClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
+use Http\Message\MessageFactory;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+/**
+ * This file is part of the msschl\monolog-http-handler package.
+ *
+ * Copyright (c) 2018 Markus Schlotbohm
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+class HttpHandler extends AbstractProcessingHandler
+{
+
+	/**
+	 * The http client instance.
+	 *
+     * @var \Http\Client\HttpClient
+     */
+    protected $client;
+
+    /**
+     * The message factory instance.
+     *
+     * @var \Http\Message\MessageFactory
+     */
+    protected $messageFactory;
+
+    /**
+     * The options array.
+     *
+     * @var array
+     */
+    protected $options = [
+    	'uri'             => null,
+    	'method'          => 'GET',
+    	'headers'         => [
+    		'Content-Type' => 'application/json'
+    	],
+    	'protocolVersion' => '1.1'
+    ];
+
+    /**
+     * Create a new monolog http client handler instance.
+     *
+     * @param  array                $options The array of options consisting of the uri, method, headers and protocol
+     *                                       version.
+     * @param  HttpAsyncClient|null $client  An instance of a psr-7 http async client implementation or null when the
+     *                                       HttpAsyncClientDiscovery should be used to find an instance.
+     * @param  MessageFactory|null  $factory An instance of a psr-7 message factory implementation or null when
+     *                                       the MessageFactoryDiscovery should be used to find an instance.
+     * @param  int                  $level   The minimum logging level at which this handler will be triggered.
+     * @param  boolean              $bubble  Whether the messages that are handled can bubble up the stack or not.
+     */
+	public function __construct(
+		array $options,
+		HttpAsyncClient $client = null,
+		MessageFactory $factory = null,
+		$level = Logger::DEBUG,
+		$bubble = true
+	) {
+		$this->client = $client ?: HttpAsyncClientDiscovery::find();
+		$this->messageFactory = $factory ?: MessageFactoryDiscovery::find();
+
+		$this->setOptions($options);
+
+		parent::__construct($level, $bubble);
+	}
+
+	/**
+	 * Sets the options for the monolog http handler.
+	 *
+	 * @param  array $options
+	 * @return \Msschl\Monolog\Handler\HttpHandler
+	 */
+	public function setOptions(array $options)
+	{
+		$this->options = array_merge($this->options, $options);
+
+		return $this;
+	}
+
+	/**
+	 * Sets the uri.
+	 *
+	 * @param  string $uri
+	 * @return \Msschl\Monolog\Handler\HttpHandler
+	 */
+	public function setUri($uri)
+	{
+		$this->options['uri'] = $uri;
+
+		return $this;
+	}
+
+	/**
+	 * Gets the uri.
+	 *
+	 * @return string
+	 */
+	public function getUri()
+	{
+		return $this->options['uri'];
+	}
+
+	/**
+	 * Sets the http method.
+	 *
+	 * @param  string $method
+	 * @return \Msschl\Monolog\Handler\HttpHandler
+	 */
+	public function setMethod($method)
+	{
+		$this->options['method'] = $method;
+
+		return $this;
+	}
+
+	/**
+	 * Gets the http method.
+	 *
+	 * @return string
+	 */
+	public function getMethod()
+	{
+		return $this->options['method'] ?: 'GET';
+	}
+
+	/**
+	 * Sets the headers.
+	 *
+	 * @param  array $headers
+	 * @return \Msschl\Monolog\Handler\HttpHandler
+	 */
+	public function setHeaders(array $headers)
+	{
+		$this->options['headers'] = $headers;
+
+		return $this;
+	}
+
+	/**
+	 * Gets the headers.
+	 *
+	 * @return array
+	 */
+	public function getHeaders() : array
+	{
+		return $this->options['headers'] ?: [ 'Content-Type' => 'application/json' ];
+	}
+
+	/**
+	 * Sets the http protocol version.
+	 *
+	 * @param  string $version
+	 * @return \Msschl\Monolog\Handler\HttpHandler
+	 */
+	public function setProtocolVersion($version = '1.1')
+	{
+		$this->options['protocolVersion'] = $version;
+
+		return $this;
+	}
+
+	/**
+	 * Gets the http protocol version.
+	 *
+	 * @return string
+	 */
+	public function getProtocolVersion()
+	{
+		return $this->options['protocolVersion'] ?: '1.1';
+	}
+
+	/**
+     * Gets the default formatter.
+     *
+     * @return \Monolog\Formatter\JsonFormatter
+     */
+    protected function getDefaultFormatter()
+    {
+        return new JsonFormatter();
+    }
+
+	/**
+     * Returns the HTTP adapter.
+     *
+     * @return \Http\Client\HttpAsyncClient
+     */
+    protected function getHttpClient(): HttpAsyncClient
+    {
+        return $this->client;
+    }
+
+    /**
+     * Returns the message factory.
+     *
+     * @return \Http\Message\MessageFactory
+     */
+    protected function getMessageFactory(): MessageFactory
+    {
+        return $this->messageFactory;
+    }
+
+    /**
+     * Writes the record.
+     *
+     * @param  array $record
+     * @return void
+     */
+    protected function write(array $record)
+    {
+    	$uri = $this->getUri();
+
+    	if (empty($uri)) {
+    		return;
+    	}
+
+    	$request = $this->getMessageFactory()->createRequest(
+    		$this->getMethod(),
+    		$this->getUri(),
+    		$this->getHeaders(),
+    		$record['formatted'],
+    		$this->getProtocolVersion()
+    	);
+
+        $this->getHttpClient()->sendAsyncRequest($request);
+    }
+}
